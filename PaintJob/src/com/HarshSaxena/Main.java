ad19e80d3b26fbfabf2172610d8176bb01962223
saxenaharsh <harsh.saxena@gmail.com>
2020-05-26 11:24:29 +0530

    Add files via upload

---
+++ PaintJob/src/com/HarshSaxena/Main.java
@@ -0,0 +1,32 @@
+package com.HarshSaxena;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        getBucketCount(-3.4,2.1,1.5,2);
+
+    }
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
+        if(width <= 0 || height <= 0 || areaPerBucket <=0 || extraBuckets <0){
+            return -1;
+        }
+        return getBucketCount(width, height, areaPerBucket) - extraBuckets;
+
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket){
+        if(width <= 0 || height <= 0 || areaPerBucket <= 0){
+            return -1;
+        }
+        return  (int) Math.ceil(width * height / areaPerBucket);
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket){
+        if(area <= 0 || areaPerBucket <= 0){
+            return -1;
+        }
+        return (int)Math.ceil(area/areaPerBucket);
+
+    }
+}
